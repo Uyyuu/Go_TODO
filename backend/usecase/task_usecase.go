9add96fc72d52a6b4c44dec8dcdefb890836e22e
Yu Usui <131258493+Uyyuu@users.noreply.github.com>
2024-08-24 07:30:55 +0000

    task_usecase Done

--- backend/usecase/task_usecase.go
+++ backend/usecase/task_usecase.go
@@ -21,27 +21,68 @@ func NewTaskUsecase(tr repository.ITaskRepository) ITaskUsecase {
 	return &taskUsecase{tr}
 }
 
-// TODO
 func (tu *taskUsecase) GetAllTasks(userId uint) ([]model.TaskResponse, error) {
-	return nil, nil
+	tasks := []model.Task{}
+	if err := tu.tr.GetAllTasks(&tasks, userId); err != nil {
+		return nil, err
+	}
+	resTasks := []model.TaskResponse{}
+	for _, task := range tasks {
+		resTask := model.TaskResponse{
+				ID 	      : task.ID,
+				Title     : task.Title,
+				CreatedAt : task.CreatedAt,
+				UpdatedAT : task.UpdatedAT,
+		}
+		resTasks = append(resTasks, resTask)
+	}
+	return resTasks, nil
 }
 
-// TODO
 func (tu *taskUsecase) GetTaskById(userId uint, taskId uint) (model.TaskResponse, error){
-	return nil, nil
+	task:= model.Task{}
+	if err := tu.tr.GetTaskById(&task, userId, taskId); err != nil {
+		return model.TaskResponse{}, err
+	}
+	reTask := model.TaskResponse{
+			ID 	      : task.ID,
+			Title     : task.Title,
+			CreatedAt : task.CreatedAt,
+			UpdatedAT : task.UpdatedAT,
+	}
+	return reTask, nil
 }
 
-// TODO: なんで引数にuser_idないのか？
 func (tu *taskUsecase) CreateTask(task model.Task) (model.TaskResponse, error) {
-	return nil, nil
+	if err := tu.tr.CreateTask(&task); err != nil {
+		return model.TaskResponse{}, nil
+	}
+	reTask := model.TaskResponse{
+			ID 	      : task.ID,
+			Title     : task.Title,
+			CreatedAt : task.CreatedAt,
+			UpdatedAT : task.UpdatedAT,
+	}
+	return reTask, nil
 }
 
-// TODO
+// TODO: これtaskを値渡しして，UpdateTaskでは参照渡しにすることで，変更前と変更後の情報をどっちも保持できる？
 func (tu *taskUsecase) UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error) {
-	return nil, nil
+	if err := tu.tr.UpdateTask(&task, userId, taskId); err != nil {
+		return model.TaskResponse{}, nil
+	}
+	reTask := model.TaskResponse{
+			ID 	      : task.ID,
+			Title     : task.Title,
+			CreatedAt : task.CreatedAt,
+			UpdatedAT : task.UpdatedAT,
+	}
+	return reTask, nil
 }
 
-// TODO
 func (tu* taskUsecase) DeleteTask(userId uint, taskId uint) error {
+	if err := tu.tr.DeleteTask(userId, taskId); err != nil {
+		return err
+	}
 	return nil
 }
